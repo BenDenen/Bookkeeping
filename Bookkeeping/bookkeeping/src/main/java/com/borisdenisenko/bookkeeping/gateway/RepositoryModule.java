af1cad9e47acc39debef525939b37a4fc373cb07
Shumer512 <Shumer512@gmail.com>
2017-02-21 01:14:40 +0300

    Change Gradle config

--- Bookkeeping/bookkeeping/src/main/java/com/borisdenisenko/bookkeeping/gateway/RepositoryModule.java
+++ Bookkeeping/bookkeeping/src/main/java/com/borisdenisenko/bookkeeping/gateway/RepositoryModule.java
@@ -1,13 +1,16 @@
 package com.borisdenisenko.bookkeeping.gateway;
 
+import com.borisdenisenko.bookkeeping.AppModule;
 import com.borisdenisenko.bookkeeping.gateway.web.WebApiRepositoryImpl;
 
+import dagger.Module;
 import dagger.Provides;
 
 /**
  * Created by bdenisenko on 16.02.2017.
  */
 
+@Module(includes = AppModule.class)
 public class RepositoryModule {
 
     @Provides
